47afa42d7b3fa612a4ef47ad114daefb7bdb1188
Alex Flores <domain@me.com>
2019-09-23 21:43:18 -0400

    adds email icon that links to the HIBP view

--- src/components/Result.svelte
+++ src/components/Result.svelte
@@ -3,9 +3,18 @@
 	import { link } from "svelte-spa-router"
 </script>
 
+<style>
+.clip {
+	margin-bottom: 0px;
+}
+</style>
+
 <tr>
-	<td>
-		<a href="/username/{result.username}" use:link> {result.username} </a>
+	<td class="valign-wrapper row clip">
+		<a class ="col s10" href="/username/{result.username}" use:link> {result.username} </a>
+		<a class="" href="/email/{result.username}@{result.domain}" use:link>
+			<i class="col s1 material-icons">email</i>
+		</a>
 	</td>
 	<td>
 		<a href="/domain/{result.domain}" use:link> {result.domain} </a>
